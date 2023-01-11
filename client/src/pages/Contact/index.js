da70dae4d13f34753a8ac4fa1efaaace41d5a29f
Alexander Carr <alexanderc93@gmail.com>
2023-01-10 19:34:27 -0800

    add base portfolio as frontent in client section

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,58 +1,60 @@
-import React, {useState} from "react";
-import nodemailer from "nodemailer";
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-let transporter = nodemailer.createTransport({
-  host: "send.smtp.mailtrap.io",
-  port: 587,
-  auth: {
-    user: process.env.mailUser,
-    pass: process.env.mailPass,
-  }
-})
+import React, { useState, } from "react";
 
 export default function Contact() {
-  let name = useState();
-  let message = useState();
-  let email = useState();
 
-  function handleSumbmit() {
-    sendEmail(name, email, message);
-  }
+  let [contactData, setContactData] = useState({
+    name: "",
+    message: "",
+    email: "",
+  });
 
-  function sendEmail(name, email, message) {
-    transporter.sendMail(message, (err, info) => {
-      if (err) {
-        console.log(err)
-      }
-      else {
-        console.log(info)
-      }
+  function handleSubmit(e) {
+    e.preventDefault();
+    // request api with data
+
+    fetch("/api/email", {
+      method: "POST",
+      mode: "same-origin",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: contactData.name,
+        message: contactData.message,
+        email: contactData.email
+      })
     })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
   }
 
-  function handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value    });
+  function handleInputChange(e) {
+    setContactData(contactData => ({
+      ...contactData,
+      [e.target.name]: e.target.value
+    }));
   }
 
   return (
     <div className="contact">
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group p-4">
-            <label htmlFor="name" className="mb-2">Name</label>
-            <input className="form-control mb-4" id="name" type="name" onChange={handleInputChange} value={name}/>
-            <label htmlFor="email" className="mb-2">Email</label>
-            <input className="form-control mb-4" id="email" type="email" onChange={handleInputChange} value={email} />
-            <label htmlFor="message" className="mb-2">Message</label>
-            <textarea className="form-control mb-4" name="message" id="contactMessage" cols="30" rows="10" placeholder="Input your message" value={message} />
-            <button type="button" className="btn form-control btn-success">Send</button>
-          </div>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group p-4">
+          <label htmlFor="name" className="mb-2">Name</label>
+          <input className="form-control mb-4" name="name" id="name" type="name" onChange={handleInputChange} value={contactData.name} />
+          <label htmlFor="email" className="mb-2">Email</label>
+          <input className="form-control mb-4" name="email" id="email" type="email" onChange={handleInputChange} value={contactData.email} />
+          <label htmlFor="message" className="mb-2">Message</label>
+          <textarea className="form-control mb-4" name="message" id="contactMessage" cols="30" rows="10" placeholder="Input your messages" onChange={handleInputChange} value={contactData.message} />
+          <button type="submit" className="btn form-control btn-success">Send</button>
+        </div>
+      </form>
     </div>
   )
 }
