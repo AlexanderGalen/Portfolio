52de852b904321e9507cf5965a18a0fcd5382a6a
Alexander Carr <alexanderc93@gmail.com>
2023-01-10 20:42:51 -0800

    add modal for successful sent message on contact page

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,12 +1,22 @@
-import React, { useState, } from "react";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button"
 
 export default function Contact() {
-
+  
+  let [modalVisible, setModalVisible] = useState(false);
   let [contactData, setContactData] = useState({
     name: "",
     message: "",
     email: "",
   });
+
+  function handleClose(){
+    setModalVisible(false);
+  }
+  function handleOpen() {
+    setModalVisible(true);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -32,6 +42,9 @@ export default function Contact() {
       .catch((err) => {
         console.log(err);
       })
+      
+
+      handleOpen();
 
   }
 
@@ -43,18 +56,30 @@ export default function Contact() {
   }
 
   return (
-    <div className="contact">
+    <section className="contact">
+      
       <form onSubmit={handleSubmit}>
-        <div className="form-group p-4">
+        <section className="form-group p-4">
           <label htmlFor="name" className="mb-2">Name</label>
           <input className="form-control mb-4" name="name" id="name" type="name" onChange={handleInputChange} value={contactData.name} />
           <label htmlFor="email" className="mb-2">Email</label>
           <input className="form-control mb-4" name="email" id="email" type="email" onChange={handleInputChange} value={contactData.email} />
           <label htmlFor="message" className="mb-2">Message</label>
           <textarea className="form-control mb-4" name="message" id="contactMessage" cols="30" rows="10" placeholder="Input your messages" onChange={handleInputChange} value={contactData.message} />
-          <button type="submit" className="btn form-control btn-success">Send</button>
-        </div>
+          <Button variant="success" type="submit">Send</Button>
+        </section>
       </form>
-    </div>
+      <Modal show={modalVisible} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Message Sent!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Thank you for your message, I'll be in touch!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleClose}>
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </section>
   )
 }
