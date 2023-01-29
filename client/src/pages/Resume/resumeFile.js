0b51153a573a9a4f8539acc6e5c97a7693c7b75d
Alexander Carr <alexanderc93@gmail.com>
2023-01-28 18:51:44 -0800

    update resume display to use html and css since i finally found a good converter

--- client/src/pages/Resume/resumeFile.js
+++ client/src/pages/Resume/resumeFile.js
@@ -1,8 +1,38 @@
 
+import "./style.css" 
+
 export default function ResumeFile() {
 
 	return(
-		<section>
-			 <iframe src="./resume.pdf#toolbar=0" width="100%" height="800px" frameborder="0"></iframe>
-		</section>
+			<div id="page_1">
+
+
+			<p className="p0 ft0">Alexander Carr</p>
+			<p className="p1 ft1">Nashville, TN</p>
+			<p className="p1 ft1">alexanderc93@gmail.com | 619 938 5857</p>
+			<p className="p1 ft3"><a href="http://www.linkedin.com/in/alexcarr93"><span className="ft2">linkedin.com/in/alexcarr93</span></a><span className="ft1"> | </span><a href="https://github.com/AlexanderGalen"><span className="ft2">github.com/AlexanderGalen</span></a><span className="ft1"> | </span><a href="http://alexandergalen.com/portfolio"><span className="ft2">alexandergalen.com</span></a></p>
+			<p className="p2 ft5"><span className="ft4">Software Engineer </span>with 8 years experience in software and technical productions, specializing in automation, with some web development experience. Open to working <nobr>in-person</nobr> or remote.</p>
+			<p className="p3 ft6">Skills</p>
+			<p className="p4 ft5"><span className="ft1">●</span><span className="ft7">Profcient with modern javascript and React; Confdent with full stack development using both MERN and LAMP stacks.</span></p>
+			<p className="p5 ft1"><span className="ft1">●</span><span className="ft8">Excellent verbal and written communication skills.</span></p>
+			<p className="p6 ft1"><span className="ft1">●</span><span className="ft8">Thrives under pressure, Innovative problem solver.</span></p>
+			<p className="p7 ft6">Projects</p>
+			<p className="p8 ft11"><span className="ft1">●</span><span className="ft9">Chattable</span><span className="ft5">: </span><a href="https://github.com/AlexanderGalen/Chattable"><span className="ft10">github.com/AlexanderGalen/Chattable</span></a><span className="ft5"> | </span><a href="http://chat.alexandergalen.com"><span className="ft10">chat.alexandergalen.com</span></a><span className="ft5"> Worked with four teammates over one week to develop an instant messaging app for diferent users to be able to instantly communicate with each other. Uses Express as a server, MySQL as a database with sequelize as an ORM, and Socket.io for instant message transmission.</span></p>
+			<p className="p9 ft15"><span className="ft1">●</span><span className="ft12">Cats vs Dogs</span><span className="ft13">: </span><nobr><a href="https://github.com/AlexanderGalen/Cats-vs-Dogs"><span className="ft14">github.com/AlexanderGalen/Cats-vs-Dogs</span></a></nobr><span className="ft13"> | </span><a href="http://game.alexandergalen.com"><span className="ft14">game.alexandergalen.com</span></a><span className="ft13"> Worked with a team of fve over one week to create a web clone of the game “codewords”. Uses</span></p>
+			<p className="p10 ft1">a MERN stack, along with Socket.io for game events, and GraphQL on top of MongoDB.</p>
+			<p className="p11 ft3"><span className="ft1">●</span><span className="ft16">NutriTech</span><span className="ft1">: </span><a href="https://github.com/AlexanderGalen/NutriTech"><span className="ft2">github.com/AlexanderGalen/NutriTech</span></a><span className="ft1"> | </span><a href="http://nutritech.alexandergalen.com"><span className="ft2">nutritech.alexandergalen.com</span></a></p>
+			<p className="p12 ft13">Worked with three teammates over the course of a week to create this simple front end app designed to allow users to search for meals with a keyword and receive a recipe including those keywords as well as nutritional information for that dish displayed on a graph.</p>
+			<p className="p3 ft6">Experience</p>
+			<p className="p7 ft6">Automation Engineer @ Graphic Business Solutions (<span className="ft17">2013 - 2019)</span></p>
+			<p className="p13 ft5">Designed, built, and maintained custom internal software and user interfaces for a large marketing stationary production company in San Diego.</p>
+			<p className="p5 ft1"><span className="ft1">●</span><span className="ft8">Built apps, scripts, and sites with JS, NodeJS, React, PHP, MySQL and other tools.</span></p>
+			<p className="p14 ft5"><span className="ft1">●</span><span className="ft7">Redesigned and updated the internal intranet from using classNameic ASP.net to modern tech stacks, including React, for increased stability and extensibility.</span></p>
+			<p className="p15 ft5"><span className="ft1">●</span><span className="ft7">Collaborated directly with the CIO on solutions for production line efciency and designing software for new product lines.</span></p>
+			<p className="p16 ft5"><span className="ft1">●</span><span className="ft7">Increased rate of production 10x by introducing automation to historically analog production lines.</span></p>
+			<p className="p17 ft5"><span className="ft1">●</span><span className="ft7">Researched and employed leading web technology to solve constant new problems in a fast paced work environment.</span></p>
+			<p className="p5 ft1"><span className="ft1">●</span><span className="ft8">Attended React Conference 2018 to bring latest best practices into the org</span></p>
+			<p className="p18 ft4">Software Engineer (Volunteer) @ Youth Venture Teen Centers <span className="ft5">Worked with a team of four volunteers to create a customized web based </span><nobr><span className="ft5">check-in</span></nobr><span className="ft5"> system for a </span><nobr><span className="ft5">non-proft</span></nobr><span className="ft5"> teen center, using the Angular.js framework, PHP, and PostgreSQL.</span></p>
+			<p className="p19 ft6">Education</p>
+			<p className="p20 ft13"><span className="ft1">●</span><span className="ft18">UCSD Extended Studies, San Diego, CA Full Stack Web Development Certifcate</span></p>
+			</div>
 	)}
